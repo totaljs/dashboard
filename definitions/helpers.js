5050e33c53aa3cb60ece9ab3987362d2f38e19d1
Peter Širka <petersirka@gmail.com>
2016-12-24 12:23:06 +0100

    Improve design and code.

--- definitions/helpers.js
+++ definitions/helpers.js
@@ -1,7 +1,3 @@
 F.helpers.makerepository = function(m) {
-	if (!m.key)
-		return m.url;
-	if (m.url.indexOf('?') === -1)
-		return m.url + '?key=' + m.key;
-	return m.url + '&key=' + m.key;
+	return !m.key ? m.url : m.url.indexOf('?') === -1 ? m.url + '?key=' + m.key : m.url + '&key=' + m.key;
 };
