d3a652a7c91b857a16fcdafa064901739229972e
Peter Širka <petersirka@gmail.com>
2017-01-06 12:08:41 +0100

    Update text.

--- public/widgets/process.js
+++ public/widgets/process.js
@@ -93,7 +93,7 @@ WIDGET('Process', function() {
 
 				tooltip = e.target;
 				var el = d3.select(e.target);
-				self.tooltip(e.target, 'Memory: <b>{0}</b><br />CPU: <b>{1}%</b><br />Created: <b>{2}</b>'.format(item[key_memory].filesize(), item[key_cpu], item.updated.parseDate().format('yyyy-MM-dd HH:mm')), 180);
+				self.tooltip(e.target, 'RSS: <b>{0}</b><br />CPU: <b>{1}%</b><br />Created: <b>{2}</b>'.format(item[key_memory].filesize(), item[key_cpu], item.updated.parseDate().format('yyyy-MM-dd HH:mm')), 180);
 			});
 			(size.device === 'xs' || size.device === 'lg') && this.append('text').attr('transform', 'translate(8,0)').attr('text-anchor', 'middle').attr('x', function(d, i) { return x(i); }).attr('y', height - 3).text(function(d) { return d.hour; });
 		});
